797fd12cd287e43d11eb2fbd5167ab28b97aabf5
trimmerpop <trimmerpop@gmail.com>
2025-12-23 01:55:30 +0900

    fixed icon, file delete.

--- const.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.const import Platform
-
-DOMAIN = "solar_inverter_rems"
-CONF_SLAVE_ID = "slave_id"
-DEFAULT_NAME = "Solar Inverter"
-DEFAULT_SCAN_INTERVAL = 600
-DEFAULT_PORT = 8899
-PLATFORMS = [Platform.SENSOR]
